d9121009ed510f199f40ae32600c792350f23250
iassadki <ilias.assadki@gmail.com>
2024-03-28 14:55:51 +0100

    Liked songs

--- screens/LikedSongsScreen.js
+++ screens/LikedSongsScreen.js
@@ -1,10 +1,27 @@
+// LikedSongsScreen.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const songs = [
+    { id: 1, title: 'Song 1' },
+    { id: 2, title: 'Song 2' },
+    { id: 3, title: 'Song 3' },
+    // Ajoutez plus de chansons si nécessaire
+];
 
 export default function LikedSongsScreen() {
     return (
         <View style={styles.container}>
-            <Text> Liked songs </Text>
+            <Text style={styles.header}>Liked songs</Text>
+            <FlatList
+                data={songs}
+                renderItem={({ item }) => (
+                    <View style={styles.songItem}>
+                        <Text>{item.title}</Text>
+                    </View>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+            />
         </View>
     );
 }
@@ -12,8 +29,19 @@ export default function LikedSongsScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'white', // changer 'color' à 'backgroundColor'
-        justifyContent: 'center',
+        backgroundColor: 'white',
+        padding: 10,
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    songItem: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        marginBottom: 10,
+        borderRadius: 5,
     },
 });
